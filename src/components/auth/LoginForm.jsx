49bbd66119bbdd7bf86977a4cb27c3f0135caabf
blbilu <90701705+blbilu@users.noreply.github.com>
2021-09-16 02:38:45 +0530

    Update LoginForm.jsx

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBInput } from 'mdbreact';
-import {login,register,loginWithJWT, getJWT} from '../../service/auth';
-import {useHistory, Redirect} from 'react-router-dom';
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBInput } from "mdbreact";
+import { login, register, loginWithJWT, getJWT } from "../../service/auth";
+import { useHistory, Redirect } from "react-router-dom";
 import Joi from "joi-browser";
 import { toast } from "react-toastify";
 
@@ -11,45 +11,45 @@ const schemaLogin = {
 };
 
 const schemaRegister = {
-    name: Joi.string().required().min(5).max(50).label("Name"),
-    email: Joi.string()
+  name: Joi.string().required().min(5).max(50).label("Name"),
+  email: Joi.string()
     .required()
     .min(5)
     .max(255)
     .email({ minDomainAtoms: 2 })
     .label("Email"),
-    password:Joi.string().required().min(5).max(255), 
+  password: Joi.string().required().min(5).max(255),
 };
 
 const LoginForm = () => {
   const history = useHistory();
-  const [state,setState] = useState({
-    isLogin : true,
-    form : {
-      email : '',
-      password : '',
-      name : ''
-    }
-  })
+  const [state, setState] = useState({
+    isLogin: true,
+    form: {
+      email: "",
+      password: "",
+      name: "",
+    },
+  });
 
   //change handler
-  const changeHandler = e =>{
-    const {name,value} = e.target;
-    setState(prev=>({
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setState((prev) => ({
       ...prev,
-      form : {
+      form: {
         ...prev.form,
-        [name] : value
-      }
-    }))
-  }
+        [name]: value,
+      },
+    }));
+  };
 
   //auth mode change
-  const switchMode = ()=>{
-    setState(prev=>({
+  const switchMode = () => {
+    setState((prev) => ({
       ...prev,
-      isLogin : !prev.isLogin
-    }))
+      isLogin: !prev.isLogin,
+    }));
   };
 
   //validation
@@ -58,52 +58,61 @@ const LoginForm = () => {
       abortEarly: false,
     };
 
-    let errorLog ={};
-    
+    let errorLog = {};
+
     if (state.isLogin) {
-      const { error } = Joi.validate({email:state.form.email, password:state.form.password}, schemaLogin, options);
+      const { error } = Joi.validate(
+        { email: state.form.email, password: state.form.password },
+        schemaLogin,
+        options
+      );
       errorLog = error;
-    }else {
+    } else {
       const { error } = Joi.validate(state.form, schemaRegister, options);
       errorLog = error;
     }
-    
+
     if (!errorLog) return null;
-    
+
     const errors = {};
     for (let item of errorLog.details) errors[item.path[0]] = item.message;
-      return errors;
+    return errors;
   };
 
   //form submit form
-  const submitForm =async e =>{
+  const submitForm = async (e) => {
     e.preventDefault();
-    
+
     const errors = validate();
 
-    if(state.isLogin){
+    if (state.isLogin) {
       if (errors) {
         toast.error("Your credentials are not correct, please try again.");
         return false;
       }
-      login(state.form.email,state.form.password).then(res=>{
-        if(res === true){
-            history.push('/Home')
-        }else{
-          toast.error('Please check the credentials')
+      login(state.form.email, state.form.password).then((res) => {
+        if (res === true) {
+          history.push("/Home");
+        } else {
+          toast.error("Please check the credentials");
         }
-      })
-    }else{
-      
+      });
+    } else {
       if (errors) {
         toast.error(Object.values(errors).join(", "));
         return false;
       }
-      const response = await register(state.form);
-      loginWithJWT(response.headers['x-auth-token']);
-      history.push('/Home')
+
+      try {
+        const response = await register(state.form);
+        loginWithJWT(response.headers["x-auth-token"]);
+        history.push("/Home");
+      } catch (error) {
+        toast.error("Email already exists!");
+        return null;
+      }
     }
-  }
+  };
 
   if (getJWT()) {
     return <Redirect exact to="/Home" />;
@@ -112,60 +121,60 @@ const LoginForm = () => {
   return (
     <MDBContainer>
       <MDBRow className="login-box">
-        <MDBCol md='6'>
+        <MDBCol md="6">
           <MDBCard
-            className='card-image'
+            className="card-image"
             style={{
               backgroundImage:
-                'url(https://mdbcdn.b-cdn.net/img/Photos/Others/pricing-table7.jpg)',
-              backgroundSize: '100%'
+                "url(https://mdbcdn.b-cdn.net/img/Photos/Others/pricing-table7.jpg)",
+              backgroundSize: "100%",
             }}
           >
-            <div className='text-white rgba-stylish-strong py-5 px-5 z-depth-4'>
-              <div className='text-center'>
-                <h3 className='white-text mb-5 mt-4 font-weight-bold'>
-                  <strong>{!state.isLogin ? 'SIGN' : 'LOG'}</strong>
-                  <a href='#!' className='green-text font-weight-bold'>
-                    <strong> {!state.isLogin ? 'UP' : 'IN'}</strong>
+            <div className="text-white rgba-stylish-strong py-5 px-5 z-depth-4">
+              <div className="text-center">
+                <h3 className="white-text mb-5 mt-4 font-weight-bold">
+                  <strong>{!state.isLogin ? "SIGN" : "LOG"}</strong>
+                  <a href="#!" className="green-text font-weight-bold">
+                    <strong> {!state.isLogin ? "UP" : "IN"}</strong>
                   </a>
                 </h3>
               </div>
               <form onSubmit={submitForm}>
-
-              {
-                !state.isLogin&&
+                {!state.isLogin && (
+                  <MDBInput
+                    label="Your name"
+                    group
+                    type="text"
+                    validate
+                    labelClass="white-text"
+                    name="name"
+                    onChange={changeHandler}
+                  />
+                )}
                 <MDBInput
-                label='Your name'
-                group
-                type='text'
-                validate
-                labelClass='white-text'
-                name="name"
-                onChange={changeHandler}
+                  label="Your email"
+                  group
+                  type="text"
+                  validate
+                  labelClass="white-text"
+                  name="email"
+                  onChange={changeHandler}
                 />
-              }
-              <MDBInput
-                label='Your email'
-                group
-                type='text'
-                validate
-                labelClass='white-text'
-                name="email"
-                onChange={changeHandler}
+                <MDBInput
+                  label="Your password"
+                  group
+                  type="password"
+                  validate
+                  labelClass="white-text"
+                  name="password"
+                  onChange={changeHandler}
                 />
-              <MDBInput
-                label='Your password'
-                group
-                type='password'
-                validate
-                labelClass='white-text'
-                name="password"
-                onChange={changeHandler}
-                />
-              <MDBRow className='d-flex align-items-center mb-4'>
-                <div className='text-center mb-3 col-md-12'>
-                  <button className="btn btn-success btn-block btn-rounded z-depth-1 waves-effect waves-light">{!state.isLogin ? 'sign up' : 'log in'}</button>
-                  {/* <MDBBtn
+                <MDBRow className="d-flex align-items-center mb-4">
+                  <div className="text-center mb-3 col-md-12">
+                    <button className="btn btn-success btn-block btn-rounded z-depth-1 waves-effect waves-light">
+                      {!state.isLogin ? "sign up" : "log in"}
+                    </button>
+                    {/* <MDBBtn
                     color='success'
                     rounded
                     type='button'
@@ -173,24 +182,23 @@ const LoginForm = () => {
                     >
                     Sign in
                   </MDBBtn> */}
-                  {/* <MDBBtn color="blue"></MDBBtn> */}
-                </div>
-              </MDBRow>
-                  </form>
-              <MDBCol md='12'>
-                <p className='font-small white-text d-flex justify-content-end'>
-                {
-                  !state.isLogin?
-                  'Have an account?':
-                  'Create new Account'
-                }
-                  
-                  <span className='green-text ml-1 font-weight-bold' style={{"cursor":"pointer"}}>
-                    {
-                      !state.isLogin?
-                      <span onClick={switchMode}>Log in</span> :
+                    {/* <MDBBtn color="blue"></MDBBtn> */}
+                  </div>
+                </MDBRow>
+              </form>
+              <MDBCol md="12">
+                <p className="font-small white-text d-flex justify-content-end">
+                  {!state.isLogin ? "Have an account?" : "Create new Account"}
+
+                  <span
+                    className="green-text ml-1 font-weight-bold"
+                    style={{ cursor: "pointer" }}
+                  >
+                    {!state.isLogin ? (
+                      <span onClick={switchMode}>Log in</span>
+                    ) : (
                       <span onClick={switchMode}>Sign Up</span>
-                    }
+                    )}
                   </span>
                 </p>
               </MDBCol>
